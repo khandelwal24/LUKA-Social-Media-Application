4acdfcfba168cccf8dbd0ba6589daf3b88c43373
Harshwardhan Khandelwal <khandelwalharsh0024@gmail.com>
2025-08-01 18:25:12 +0530

    url changed

--- Backend/Socket/Socket.js
+++ Backend/Socket/Socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:'http://localhost:1000',
+        origin:'https://luka-social-media-application-1.onrender.com',
         credentials:true,
         methods:["GET","POST"],
     }
